2ce8640bc8a720e898cabcb4ad5ec18ea055c89a
Alireza Hashemi <jelavor@gamil.com>
2025-10-21 16:28:54 +0330

    Initial Commit

---
+++ Crypto-test/Crypto_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_testApp.swift
+//  Crypto-test
+//
+//  Created by Alireza Hashemi on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
